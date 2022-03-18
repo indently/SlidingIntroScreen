9b4374b91af2bc26caeb6c894ea5fcfa0582da57
Federico <codepalace@fastmail.se>
2022-03-18 08:48:36 +0100

    Initial Commit

---
+++ SlidingIntroScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlidingIntroScreen
+//
+//  Created by Federico on 18/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlidingIntroScreen/SlidingIntroScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingIntroScreenApp.swift
+//  SlidingIntroScreen
+//
+//  Created by Federico on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SlidingIntroScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
